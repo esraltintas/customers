6adb0140a5fedf98439f8827a073060fe0db8a86
esraltintas <es.altintas@gmail.com>
2023-10-23 01:39:03 +0300

    Change background color

--- src/layout/Body.tsx
+++ src/layout/Body.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Body = styled.main`
   min-height: 100vh;
-  background-color: #f8f9fa;
+  background-color: #fff;
   font-family: "Open Sans", sans-serif;
 `;
 
